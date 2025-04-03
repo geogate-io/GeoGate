dfc336ac22fb0b6e3d48ee9b29e1749346e592fd
Ufuk Turuncoglu <turuncu@ucar.edu>
2025-04-03 10:17:53 -0600

    add more debug

--- .github/workflows/scripts/install_deps.sh
+++ .github/workflows/scripts/install_deps.sh
@@ -33,10 +33,14 @@ echo "::endgroup::"
 
 # Create config file (to fix FetchError issue)
 echo "::group::Create config.yaml"
-echo "config:" > ~/.spack/config.yaml
-echo "  url_fetch_method: curl" >> ~/.spack/config.yaml
-echo "  connect_timeout: 60" >> ~/.spack/config.yaml
-echo "  environments_root: $install_dir/spack-env" >> ~/.spack/config.yaml
+spack config add "modules:default:enable:[tcl]"
+spack config add "url_fetch_method:curl"
+spack config add "connect_timeout:60"
+spack config add "environments_root:$install_dir/spack-env"
+#echo "config:" > ~/.spack/config.yaml
+#echo "  url_fetch_method: curl" >> ~/.spack/config.yaml
+#echo "  connect_timeout: 60" >> ~/.spack/config.yaml
+#echo "  environments_root: $install_dir/spack-env" >> ~/.spack/config.yaml
 cat ~/.spack/config.yaml
 echo "::endgroup::"
 
@@ -44,12 +48,23 @@ echo "::endgroup::"
 echo "::group::Create Spack Environment and Install Dependencies"
 spack env create test
 spack env activate test
-spack add esmf@8.8.0%gcc@12.3.0+external-parallelio
+spack add lmod
+spack add libcatalyst@2.0.0%gcc@12.3.0+fortran~ipo+python
+spack --color always concretize --force --deprecated --reuse 2>&1 | tee log.concretize
+spack --color always install 2>&1 | tee log.install
+spack --color always gc -y  2>&1 | tee log.clean
+spack find -c
+. $(spack location -i lmod)/lmod/lmod/init/bash
+. spack/share/spack/setup-env.sh
+module avail
+
+#spack add esmf@8.8.0%gcc@12.3.0+external-parallelio
 #spack add libcatalyst@2.0.0%gcc@12.3.0+fortran~ipo+python
 #spack add paraview@5.13.1%gcc@12.3.0+libcatalyst+fortran~ipo+mpi+python+opengl2+cdi ^[virtuals=gl] egl ^libcatalyst@2.0.0%oneapi@2024.2.1+fortran~ipo+python
-spack --color always concretize --force --deprecated --reuse 2>&1 | tee log.concretize
-spack find -c
-spack --color always install 2>&1 | tee log.install
-spack gc -y  2>&1 | tee log.clean
-spack module tcl refresh -y
+#spack --color always concretize --force --deprecated --reuse 2>&1 | tee log.concretize
+#spack find -c
+#spack --color always install 2>&1 | tee log.install
+#spack gc -y  2>&1 | tee log.clean
+
+#spack module tcl refresh -y
 echo "::endgroup::"
